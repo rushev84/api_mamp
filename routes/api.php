5cfb28e70a75232bd2e900304d8085783cde78e5
SergeyRushev <rushevs198417@gmail.com>
2023-08-16 11:45:36 +0300

    refactor

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/posts', [PostController::class, 'index'])->name('post.index');
-Route::get('/posts/{category_id}', [PostController::class, 'category'])->name('post.category');
+Route::get('/posts/category/{category_id}', [PostController::class, 'category'])->name('post.category');
 
 Route::get('/categories', [CategoryController::class, 'index'])->name('category.index');
