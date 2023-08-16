1c874ea106330566e6323ce9de18d2f35196095a
SergeyRushev <rushevs198417@gmail.com>
2023-08-16 13:31:24 +0300

    feat: login-logout

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function enter(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            // Авторизация успешна
+            dd('success');
+//            return redirect()->route('rosters.index');
+        } else {
+            // Неверные учетные данные
+            return back()->withErrors([
+                'email' => 'Неверный email или пароль.',
+            ]);
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('login.index');
+    }
+
+}
